b09cb167648e510b5fc84178eb6ee2541a92ea23
Chandan <chandan14ns@gmail.com>
2023-12-14 15:34:12 +0530

    Added new annotations

--- src/main/java/com/hospital/controller/PatientController.java
+++ src/main/java/com/hospital/controller/PatientController.java
@@ -1,5 +1,10 @@
 package com.hospital.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients/api")
 public class PatientController {
 
 }
